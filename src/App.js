571348ce6d04a06050deb8cd1e7066a1aa0e4792
Pallav <pallavinvizag@gmail.com>
2023-09-18 17:46:49 +0530

    socket

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React from 'react'
 import Editor from './components/Editor'
 import './App.css'
 import { Box } from '@mui/material'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+import { Navbar } from "./components/Navbar"
 const App = () => {
   return (
-    <Box sx={{ background: '#f5f5f5' }}><Editor /></Box>
+    <Box sx={{ background: '#f5f5f5' }}>
+      <Navbar />
+      <Routes>
+        <Route
+          index
+          path='/'
+          exact
+          element={
+            <Navigate
+              replace
+              to={`/docs/${uuid()}`}
+            />
+          } />
+        <Route path='/docs/:id' exact element={<Editor />} />
+      </Routes>
+    </Box>
   )
 }
 
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,17 +3,11 @@ import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client'
+
 const Editor = () => {
-    const [value, setValue] = useState('');
-    const [socket, setSocket] = useState('')
-    const handleEditorChange = (content, delta, source, editor) => {
-        setValue(editor.getHTML());
-        console.log("Delta:", JSON.stringify(delta));
-        console.log(source)
-        console.log(content);
-    };
-    var toolbarOptions = [
+    const toolbarOptions = [
         ['bold', 'italic', 'underline', 'strike'],
         ['blockquote', 'code-block'],
 
@@ -32,6 +26,18 @@ const Editor = () => {
 
         ['clean']
     ];
+
+    const [value, setValue] = useState('<p>Loading...</p>');
+    const [socket, setSocket] = useState();
+    const [readOnly, setReadOnly] = useState(true);
+    const { id } = useParams()
+    const handleEditorChange = (content, delta, source, editor) => {
+        setValue(editor.getContents());
+
+        if (source !== 'user') return
+        socket && socket?.emit('change-content', editor?.getContents())
+
+    };
     const modules = {
         toolbar: toolbarOptions
     }
@@ -39,17 +45,60 @@ const Editor = () => {
     useEffect(() => {
         const socketServer = io('http://localhost:9000')
         setSocket(socketServer)
+
         return () => {
-            socketServer.disconnect()
+            socketServer?.disconnect()
         }
     }, [])
-    useEffect(() => {
 
-    }, [value])
+    useEffect(() => {
+        socket && socket?.on('receive-changes', contents => {
+            setValue(contents)
+
+        })
+        socket && socket?.on('load-document', document => {
+            setValue(document)
+            setReadOnly(false)
+        })
+        socket && socket?.emit('get-document', id)
+        let interval;
+
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [socket, id])
+
+    useEffect(() => {
+        if (!readOnly) {
+            const saveInterval = setInterval(() => {
+                socket && socket.emit('save-document', value);
+            }, 2000);
+
+            return () => {
+                clearInterval(saveInterval);
+            };
+        }
+    }, [readOnly, value, socket]);
+
     return (
         <>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', background: '#f5f5f5' }}>
-                <ReactQuill style={{ width: '100vw' }} theme="snow" value={value} onChange={handleEditorChange} modules={modules} />
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                background: '#f5f5f5',
+                position: 'relative'
+            }}>
+                <ReactQuill
+                    style={{ width: '100vw' }}
+                    theme="snow"
+                    value={value}
+                    onChange={handleEditorChange}
+                    modules={modules}
+                    readOnly={readOnly}
+                />
             </Box>
         </>
     )
