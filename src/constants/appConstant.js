3bfe220e6b350cb1178a057fa346cc8e7bfdefb4
Pallav Kumaar Patra <73886524+Pallav-19@users.noreply.github.com>
2025-04-08 01:13:56 +0530

    base url updated

--- src/constants/appConstant.js
+++ src/constants/appConstant.js
@@ -1,4 +1,4 @@
 
-export const baseURL = "https://doxy-backend-cu9t.onrender.com"
+export const baseURL = "https://musiccart-backend-o6qa.onrender.com"
 
 export const documentsApiRoute = "/api/document"
